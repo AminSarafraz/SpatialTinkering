b832ebb27683d7d3a0d1c3bc9cbd1c831e7fc00c
Amin Sarafraz <amin@computervisiononline.com>
2017-03-30 16:54:26 -0400

    changed degrees2dms to read one entry per line

--- degrees2dms.py
+++ degrees2dms.py
@@ -9,21 +9,21 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU General Public License for more details <http://www.gnu.org/licenses/>.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 # degrees2dms.py converts decimal degrees to degrees-minutes-seconds
 # 
-# usage: degrees2dms.py -f /path/to/latlong.txt
-#		An example of latlong.txt (tab delimited file)
-# 		20.012345678901230	-75.987654321012345
-# 		20.012345678901231	-75.987654321012346
-# 		20.012345678901232	-75.987654321012347
+# usage: degrees2dms.py -f /path/to/degrees.txt
+#		An example of degrees.txt
+# 		20.012345678901230
+# 		20.012345678901231
+# 		20.012345678901232
+#		-75.987654321012345
+#		-75.987654321012346
+#		-75.987654321012347
 #		...
 # note: most online converters round the numbers, this function does not! so it is much more precise. 
-# see also: 
+# see also: dms2degrees.py
 
 from __future__ import print_function
 from decimal import Decimal
@@ -36,11 +36,11 @@ ap.add_argument("-f","--file", help = "File path")
 args = vars(ap.parse_args())
 fname = args["file"]
 
-# read columns line by line and put them in a list
+# read the file line by line and put each row in a list
 with open(fname) as f:
     for line in f:
-    	latlongs = [float(col.strip()) for col in line.split('\t')]
+    	latlongs = [float(col.strip()) for col in line.split()]
     	degrees = np.fix(latlongs)
     	minutes = np.fix(np.mod(np.multiply(np.absolute(latlongs),60),60))
     	seconds = np.mod(np.multiply(np.absolute(latlongs),3600),60)
-    	print(Decimal(degrees[0]), Decimal(minutes[0]), Decimal(seconds[0]), '\t', Decimal(degrees[1]), Decimal(minutes[1]), Decimal(seconds[1]))
+    	print(Decimal(degrees[0]), Decimal(minutes[0]), Decimal(seconds[0]))
